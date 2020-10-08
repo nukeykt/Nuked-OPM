2c3f16e87d8737ebf1f6102e57fa7673d34def93
nukeykt <alexeytf2@gmail.com>
2020-10-08 20:37:15 +0900

    opm.c/h skeleton

---
+++ opm.h
@@ -0,0 +1,36 @@
+/* Nuked OPM
+ * Copyright (C) 2020 Nuke.YKT
+ *
+ * This file is part of Nuked OPM.
+ *
+ * Nuked OPM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * Nuked OPM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Nuked OPM. If not, see <https://www.gnu.org/licenses/>.
+ *
+ *  Nuked OPM emulator.
+ *  Thanks:
+ *      siliconpr0n.org(digshadow, John McMaster):
+ *          YM2151 and other FM chip decaps and die shots.
+ *
+ * version: 0.0
+ */
+#ifndef _OPM_H_
+#define _OPM_H_
+
+#include <stdint.h>
+
+
+typedef struct {
+    int cycles;
+} opm_t;
+
+#endif
